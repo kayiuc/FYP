7cfebb5c4c6810d254f77ed39dc7ea88555b2080
shleungae <shleungae@connect.ust.hk>
2018-01-05 17:41:32 +0800

    attack by j

---
+++ Assets/Attack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Attack : MonoBehaviour
+{
+
+    public GameObject objectAttack;
+    private Animation anim;
+    public bool attackStatus = false;
+
+    // Use this for initialization
+    public void Axe_attack()
+    {
+       // rb = GetComponent<Rigidbody>();
+        //anim = GetComponent<Animation>();
+        if (attackStatus == false)
+        {
+            attackStatus = true;
+        }
+        else
+            attackStatus = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float x = CrossPlatformInputManager.GetAxis("Horizontal");
+        //float y = CrossPlatformInputManager.GetAxis("Vertical");
+
+        //Vector3 movement = new Vector3(x, 0, y);
+
+        //rb.velocity = movement * 4f;
+
+        if (attackStatus == true)
+        {
+            anim.Play("axe|attack");
+        }
+    }
+}
--- Assets/DragonController.cs
+++ Assets/DragonController.cs
@@ -5,33 +5,60 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class DragonController : MonoBehaviour {
 
-	private Rigidbody rb;
+    public GameObject objectAttack;
+    public bool attackStatus = false;
+    public bool attacking = false;
+    private Rigidbody rb;
 	private Animation anim;
+
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 		anim = GetComponent<Animation> ();
 	}
+    public void Axe_attack()
+    {
+        // rb = GetComponent<Rigidbody>();
+        //anim = GetComponent<Animation>();
+        
+            attackStatus = true;
+        
+            
+    }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 		float x = CrossPlatformInputManager.GetAxis("Horizontal");
 		float y = CrossPlatformInputManager.GetAxis("Vertical");
 
 		Vector3 movement = new Vector3 (x, 0, y);
-
-		rb.velocity = movement * 2f;
-
-		if (x != 0 && y != 0) {
+        if (!anim.IsPlaying("axe|attack") && attackStatus == true && attacking == true) {
+            attackStatus = false;
+            attacking = false;
+        }
+		rb.velocity = movement * 4f;
+        if (x != 0 && y != 0) {
 			transform.eulerAngles = new Vector3 (transform.eulerAngles.x, Mathf.Atan2 (x, y) * Mathf.Rad2Deg, transform.eulerAngles.z);
 		}
 
 		if (x != 0 || y != 0) {
 			anim.Play ("axe|walk");
-		} 
-		else {
-			anim.Play ("axe|idle");
 		}
+        else if (attackStatus == true)
+        {
+            anim.Play("axe|attack");
+            attacking = true;
+        }
+        else
+        {
+            anim.Play("axe|idle");
+        }
 
-	}
+        
+
+
+
+
+    }
 }
